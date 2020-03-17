0ae785c3118eee19a9b7e659c4d92d6f68e44231
Josh Painter <jpainter4@gmail.com>
2020-03-17 14:16:38 -0700

    Styling for dialog

--- src/EventDialog/EventDialog.js
+++ src/EventDialog/EventDialog.js
@@ -4,7 +4,20 @@ import * as styled from "./styles";
 export default function EventDialog({ showDialog, xPos, yPos }) {
   return (
     <styled.Dialog showDialog={showDialog} xPos={xPos} yPos={yPos}>
-      Hello World
+      <form>
+        <div>
+          <styled.NarrowInput type="text" placeholder="Hour" name="hour" />
+          <styled.NarrowInput type="text" placeholder="Minute" name="minute" />
+        </div>
+        <styled.WideInput
+          type="text"
+          placeholder="Event name"
+          name="eventName"
+        />
+        <div>
+          <styled.SaveButton type="submit">Save</styled.SaveButton>
+        </div>
+      </form>
     </styled.Dialog>
   );
 }
--- src/EventDialog/styles.js
+++ src/EventDialog/styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const Dialog = styled.div`
-  padding: 4em;
   position: absolute;
-  border: 1px solid red;
+  padding: 1em;
   display: none;
+  background-color: royalblue;
+  border-radius: 1em;
   ${({ showDialog }) =>
     showDialog &&
     `
@@ -18,4 +19,43 @@ export const Dialog = styled.div`
     `
     top: ${yPos}px;
   `}
+`;
+
+const FormElement = `
+  display: inline-block;
+  color: black;
+  margin: .4em;
+  padding: .5em;
+  box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 2px;
+  transition: background-color 0.5s ease;
+  &:focus {
+    background-color: rgba(0,0,0,0.45);
+  }
+  ::placeholder {
+    font-size: 1.3em;
+  }
+  box-sizing: border-box;
+`;
+
+export const NarrowInput = styled.input`
+  ${FormElement}
+  width: 10em;
+`;
+
+export const WideInput = styled.input`
+  ${FormElement}
+  width: 21em;
+`;
+
+export const SaveButton = styled.button`
+  ${FormElement}
+  margin: .5em;
+  padding: 0.5em;
+  border-radius: 0.7em;
+  width: 8em;
+  color: royalblue;
+  font-size: 0.9em;
 `;
